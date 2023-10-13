60ee6365da9c815bf88908f5ac14e9cd25037d47
majdigo <majdi.ghorbel@gmail.com>
2023-10-13 13:00:59 +0200

    Initial setup of basic task classes

--- code_projet/models.py
+++ code_projet/models.py
@@ -3,46 +3,90 @@ from typing import List
 
 
 class Task:
-    """Classe représentant une tâche individuelle."""
+    """
+    Représente une tâche individuelle.
+
+    Attributs:
+        id (int): Identifiant unique pour la tâche.
+        title (str): Titre de la tâche.
+        description (str): Brève description de la tâche.
+        created_at (datetime): Date et heure de création de la tâche.
+        completed (bool): Indique si la tâche est terminée ou non.
+    """
 
     def __init__(self, title: str, description: str):
-        self.id = id(self)  # Utilisation de l'ID unique pour l'identification
+        """
+        Initialise la tâche avec un titre et une description.
+
+        Args:
+            title (str): Titre de la tâche.
+            description (str): Description de la tâche.
+        """
+        self.id = id(self)
         self.title = title
         self.description = description
         self.created_at = datetime.now()
         self.completed = False
 
     def mark_as_complete(self):
-        """Marquer la tâche comme terminée."""
+        """Marque la tâche comme étant terminée."""
         self.completed = True
 
     def __str__(self):
-        status = 'Completed' if self.completed else 'Pending'
+        """
+        Renvoie une représentation string de la tâche.
+
+        Returns:
+            str: Représentation de la tâche incluant le titre, la description et le statut.
+        """
+        status = 'Terminée' if self.completed else 'En attente'
         return f"{self.title} - {self.description} ({status})"
 
 
 class TaskList:
-    """Classe représentant une liste de tâches."""
+    """
+    Représente une liste de tâches.
+
+    Attributs:
+        tasks (List[Task]): Liste des tâches.
+    """
 
     def __init__(self):
+        """Initialise une liste de tâches vide."""
         self.tasks: List[Task] = []
 
     def add_task(self, title: str, description: str):
-        """Ajouter une nouvelle tâche à la liste."""
+        """
+        Ajoute une nouvelle tâche à la liste.
+
+        Args:
+            title (str): Titre de la tâche.
+            description (str): Description de la tâche.
+        """
         task = Task(title, description)
         self.tasks.append(task)
 
     def remove_task(self, task_id: int):
-        """Supprimer une tâche de la liste en utilisant son ID."""
+        """
+        Supprime une tâche de la liste en utilisant son ID.
+
+        Args:
+            task_id (int): ID de la tâche à supprimer.
+        """
         self.tasks = [task for task in self.tasks if task.id != task_id]
 
     def complete_task(self, task_id: int):
-        """Marquer une tâche comme terminée en utilisant son ID."""
+        """
+        Marque une tâche comme terminée en utilisant son ID.
+
+        Args:
+            task_id (int): ID de la tâche à marquer comme terminée.
+        """
         for task in self.tasks:
             if task.id == task_id:
                 task.mark_as_complete()
 
     def display_tasks(self):
-        """Afficher toutes les tâches."""
+        """Affiche toutes les tâches dans la console."""
         for task in self.tasks:
             print(task)
