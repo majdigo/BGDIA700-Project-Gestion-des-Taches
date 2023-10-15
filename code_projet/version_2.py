07ec1c69c5a5170791086570eedff912a953470d
xin <njustxuxin@outlook.com>
2023-10-15 20:01:46 +0200

    nouveau method de créer code

---
+++ code_projet/version_2.py
@@ -0,0 +1,42 @@
+import datetime as dt
+from collections import defaultdict
+
+
+class Task:
+    task_dict = defaultdict(lambda: {'description': '', 'status': 'en cours'})
+
+    def __init__(self):
+        self.nom = ''
+        self.description = ''
+
+    @classmethod
+    def add_task(cls, task, description=None):
+        if task in cls.task_dict:
+            raise ValueError('The task already exists')
+        task_info = {
+            'description': description,
+            'status': 'en cours',
+            'date': dt.datetime.now().strftime("%Y-%m-%d")
+        }
+        cls.task_dict[task].update(task_info)
+
+    @classmethod
+    def complete_task(cls, *args):
+        for arg in args:
+            task_info = cls.task_dict.get(arg)
+            if task_info:
+                task_info['status'] = 'terminée'
+
+    @classmethod
+    def delete_task(cls, *noms):
+        for nom in noms:
+            cls.task_dict.pop(nom, None)
+
+    @classmethod
+    def display(cls):
+        task_names = [
+                        task
+                        for task, info in cls.task_dict.items()
+                        if info['status'] == 'en cours'
+                        ]
+        print(task_names)
