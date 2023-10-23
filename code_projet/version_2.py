df9f1ba98dbb73232018022b56c4e3bec6e570c9
xin <njustxuxin@outlook.com>
2023-10-23 18:05:36 +0200

    ajout of logging and change the code style

--- code_projet/version_2.py
+++ code_projet/version_2.py
@@ -1,15 +1,19 @@
 import datetime as dt
 from collections import defaultdict
+import logging
+
+# Create a logger
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
 
 
 class Task:
-    #Create a dictionary to store tasks
+    # Create a dictionary to store tasks
     task_dict = defaultdict(lambda: {'description': '', 'status': 'en cours'})
 
     def __init__(self):
         self.nom = ''
         self.description = ''
-        #initialize the task dictionary by each call of the class
+        # initialize the task dictionary by each call of the class
         Task.reset_dict()
 
     @classmethod
@@ -24,31 +28,37 @@ class Task:
             ValueError: _description_
         """
         if task in cls.task_dict:
-            raise ValueError('The task already exists')  
+            raise ValueError('The task already exists')
         task_info = {
             'description': description,
             'status': 'en cours',
             'date': dt.datetime.now().strftime("%Y-%m-%d")
         }
         cls.task_dict[task].update(task_info)
+        # Ajouter la journalisation
+        logging.info(f"Added task: {task}, Description: {description}")
 
     @classmethod
     def complete_task(cls, *args):
-        #complete one or more tasks
+        # complete one or more tasks
         for arg in args:
             task_info = cls.task_dict.get(arg)
             if task_info:
                 task_info['status'] = 'terminée'
+        # Ajouter la journalisation
+        logging.info(f"Completed task(s): {', '.join(args)}")
 
     @classmethod
     def delete_task(cls, *noms):
-        #delete one or more tasks
+        # delete one or more tasks
         for nom in noms:
             cls.task_dict.pop(nom, None)
+        # Ajouter la journalisation
+        logging.info(f"Deleted task(s): {', '.join(noms)}")
 
     @classmethod
     def display(cls):
-        #display all tasks which are not completed
+        # display all tasks which are not completed
         task_names = [
                         task
                         for task, info in cls.task_dict.items()
@@ -58,15 +68,27 @@ class Task:
 
     @classmethod
     def reset_dict(cls):
-        #reset the task dictionary
+        # reset the task dictionary
         cls.task_dict.clear()
 
     @classmethod
     def clear_all(cls):
-        #clear all tasks
-        cls.reset_dict()   
+        # clear all tasks
+        cls.reset_dict()
 
-if __name__ =="__main__":
-    task=Task()
+
+if __name__ == "__main__":
+    # Configurer la journalisation vers le fichier et la console
+    logging.basicConfig(filename='myapp.log',
+                        level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.
+                                 Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logging.getLogger('').addHandler(console_handler)
+
+    task = Task()
     task.add_task('task1')
     print(task.task_dict)
