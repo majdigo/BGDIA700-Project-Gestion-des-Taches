cbc2d4d6da1bfb68830f6642fa69811f2b6cd2a7
xin <njustxuxin@outlook.com>
2023-10-23 12:59:45 +0200

    ajout of documetation

--- code_projet/version_2.py
+++ code_projet/version_2.py
@@ -3,15 +3,26 @@ from collections import defaultdict
 
 
 class Task:
+    #Create a dictionary to store tasks
     task_dict = defaultdict(lambda: {'description': '', 'status': 'en cours'})
 
     def __init__(self):
         self.nom = ''
         self.description = ''
+        #initialize the task dictionary by each call of the class
         Task.reset_dict()
 
     @classmethod
     def add_task(cls, task, description=None):
+        """_summary_
+
+        Args:
+            task (_type_): _description_
+            description (_type_, optional): _description_. Defaults to None.
+
+        Raises:
+            ValueError: _description_
+        """
         if task in cls.task_dict:
             raise ValueError('The task already exists')  
         task_info = {
@@ -23,6 +34,7 @@ class Task:
 
     @classmethod
     def complete_task(cls, *args):
+        #complete one or more tasks
         for arg in args:
             task_info = cls.task_dict.get(arg)
             if task_info:
@@ -30,11 +42,13 @@ class Task:
 
     @classmethod
     def delete_task(cls, *noms):
+        #delete one or more tasks
         for nom in noms:
             cls.task_dict.pop(nom, None)
 
     @classmethod
     def display(cls):
+        #display all tasks which are not completed
         task_names = [
                         task
                         for task, info in cls.task_dict.items()
@@ -44,8 +58,15 @@ class Task:
 
     @classmethod
     def reset_dict(cls):
+        #reset the task dictionary
         cls.task_dict.clear()
 
     @classmethod
     def clear_all(cls):
+        #clear all tasks
         cls.reset_dict()   
+
+if __name__ =="__main__":
+    task=Task()
+    task.add_task('task1')
+    print(task.task_dict)
